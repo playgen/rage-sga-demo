9549cd104c6ec4dfcac9a7c519a8147750105680
Ben Hirsh <b.en@playgen.com>
2016-02-12 11:20:50 +0000

    adding countdown timer
    Former-commit-id: 7e8b4b3c6abaca2496b2be4090c211e942b753cd

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimerScript : MonoBehaviour {
+
+    private Text timeText;
+    private float timeLeft;
+    public float startTime;
+    private bool startTimer = false;
+
+	// Use this for initialization
+	void Start () {
+        timeText = GetComponent<Text>();
+        timeLeft = startTime;
+	}
+    void OnPlayerConnected(NetworkPlayer player)
+    {
+        Debug.Log("Player Connected3");
+    }
+    public void StartTimer()
+    {
+        startTimer = true;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (startTimer)
+        {
+            timeText.text = timeLeft.ToString();
+            timeLeft -= Time.deltaTime;
+            if (timeLeft <= 0)
+            {
+                startTimer = false;
+            }
+        }
+           
+	}
+}
