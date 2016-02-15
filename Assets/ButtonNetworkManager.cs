8f5ceb573164a5b8898bd4782cbd32b4e764a25b
Matthieu Lepinay <matt@playgen.com>
2016-02-15 09:14:11 +0000

    Initial SGA implementation
    Former-commit-id: 52712209cb656b6fcf08e357a791c78720b9dfbf

--- Assets/ButtonNetworkManager.cs
+++ Assets/ButtonNetworkManager.cs
@@ -1,23 +1,44 @@
-﻿using UnityEngine;
+﻿using SocialGamification;
 using System.Collections;
+using UnityEngine;
 using UnityEngine.Networking;
 
-public class ButtonNetworkManager : NetworkManager {
-
+public class ButtonNetworkManager : NetworkManager
+{
 	// Use this for initialization
-	void Start () {
-	    
+	private void Start()
+	{
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	private void Update()
+	{
 	}
 
-    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-    {
-        base.OnServerAddPlayer(conn, playerControllerId);
-        Debug.Log("New player " + playerControllerId);
-    }
-
-
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		base.OnServerAddPlayer(conn, playerControllerId);
+		if (numPlayers == 3)
+		{
+			Match.Load("0", true, string.Empty, (Match[] matches) =>
+			{
+				if (matches.Length == 0)
+				{
+					Debug.Log("No match");
+				}
+				else
+				{
+					foreach (Match match in matches)
+					{
+						if (!match.finished)
+						{
+							Debug.Log("Found match with " + match.users);
+							ServerManager.CurrentMatch = match;
+							return;
+						}
+					}
+				}
+			});
+		}
+	}
 }
