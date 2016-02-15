b1d2db85eb3335c2b6992a3a890d15b137a3090a
Matthieu Lepinay <matt@playgen.com>
2016-02-15 09:14:11 +0000

    Initial SGA implementation
    Former-commit-id: 25e112844e3c09ed32b8906ebf4a6d1f273fec1d

---
+++ Assets/ServerManager.cs
@@ -0,0 +1,76 @@
+﻿using SocialGamification;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerManager : NetworkBehaviour
+{
+	public static int IsSearching = 0;
+
+	public static Match CurrentMatch { get; set; }
+
+	// Use this for initialization
+	private void Start()
+	{
+		string username = "matt";
+		string password = "matt";
+
+		if (isServer)
+		{
+			username = "mayur";
+			password = "mayur";
+		}
+		SocialGamificationManager.platform.Authenticate<User>(username, password, (bool success, string error) =>
+		{
+			if (isServer)
+			{
+				SetIsSearching(1);
+			}
+			JoinMatch();
+		});
+	}
+
+	public static void SetIsSearching(int val)
+	{
+		SocialGamificationManager.localUser.customData["isSearching"] = val;
+		SocialGamificationManager.localUser.Update((bool success, string error) =>
+		{
+			if (success)
+			{
+				IsSearching = val;
+			}
+			else
+			{
+				SocialGamificationManager.localUser.customData["isSearching"] = IsSearching;
+			}
+		});
+	}
+
+	private void JoinMatch()
+	{
+		if (!isServer)
+		{
+			SearchCustomData[] searchData = {
+			new SearchCustomData("isSearching", eSearchOperator.Equals, "1")
+		};
+
+			Match.QuickMatch(false, searchData, 1, (Match match) =>
+			{
+				if (match == null)
+				{
+					Debug.Log("No Match");
+				}
+				else
+				{
+					CurrentMatch = match;
+					Debug.Log("Made match: " + match);
+				}
+			});
+		}
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+}
