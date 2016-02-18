0a341b4a0693a478fc9fd2ab587fe8c2c8d5d735
Ben Hirsh <b.en@playgen.com>
2016-02-18 10:28:38 +0000

    fixed game start with SGA match start synch. Now to implement goals and achievements
    Former-commit-id: ae6e587e09b51c1c4670d3cee9ab9c213b2634d9

--- Assets/ButtonNetworkManager.cs
+++ Assets/ButtonNetworkManager.cs
@@ -18,30 +18,5 @@ public class ButtonNetworkManager : NetworkManager
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
 	{
 		base.OnServerAddPlayer(conn, playerControllerId);
-        Debug.Log("NumPlayers: " + numPlayers);
-		if (numPlayers == 2)
-		{
-			Match.Load("0", true, string.Empty, (Match[] matches) =>
-			{
-				if (matches.Length == 0)
-				{
-					Debug.Log("No match");
-				}
-				else
-				{
-					foreach (Match match in matches)
-					{
-						if (!match.finished)
-						{
-							Debug.Log("Found match with " + match.users);
-							ServerManager.currentMatch = match;
-                            //Start Game
-                            GameController.controller.GetComponent<GameController>().StartGame();
-							return;
-						}
-					}
-				}
-			});
-		}
 	}
 }
